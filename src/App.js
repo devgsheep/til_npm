f69c305b3def6e9fee2f5fcce2243bb445ed8bcf
devgsheep <dev.gsheep@gmail.com>
2025-07-30 11:51:02 +0900

    [docs] Ant Desing의 이해

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Header from "./components/layout/Header";
-import Slide from "./pages/Slide";
+import JoinPage from "./pages/JoinPage";
 
 function App() {
+  // js 자리
+  // 1. 초기값
+
+  // 2. 라벨넣기
+
+  // 3. placeholder 넣기
+
+  // 4. 필수값 표현하기
+
+  // 5. 필수값 안내 메세지 표시하기
+
+  // 6. 각 필드의 입력중인 값 알아내기
+
+  // 7. 확인 버튼 클릭 시 최종 입력값
+
+  // jsx 자리
   return (
     <div>
       <Header />
-      <Slide />
+      <JoinPage />
     </div>
   );
 }
