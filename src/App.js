ccadbeca4704f8af7e5628800f8d512dac136ea9
devgsheep <dev.gsheep@gmail.com>
2025-07-31 10:01:32 +0900

    [docs] Recoil의 이해

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import Header from "./components/layout/Header";
-import JoinPage from "./pages/JoinPage";
+import CounterAtom from "./components/CounterAtom";
+import TodoList from "./components/TodoList";
 
 function App() {
-  // js 자리
-  // 1. 초기값
-
-  // 2. 라벨넣기
-
-  // 3. placeholder 넣기
-
-  // 4. 필수값 표현하기
-
-  // 5. 필수값 안내 메세지 표시하기
-
-  // 6. 각 필드의 입력중인 값 알아내기
-
-  // 7. 확인 버튼 클릭 시 최종 입력값
-
-  // jsx 자리
   return (
     <div>
-      <Header />
-      <JoinPage />
+      <CounterAtom />
+      <TodoList />
     </div>
   );
 }
